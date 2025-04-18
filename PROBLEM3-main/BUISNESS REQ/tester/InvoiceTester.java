3c638664913910ef52354b19e91f2fcb10b6fc0a
SvaraShah <svarashah215@gmail.com>
2025-04-18 18:11:30 +0530

    Update InvoiceTester.java

--- PROBLEM3-main/BUISNESS REQ/tester/InvoiceTester.java
+++ PROBLEM3-main/BUISNESS REQ/tester/InvoiceTester.java
@@ -6,10 +6,10 @@ import util.ReadData;
 
 public class InvoiceTester {
     public static void readInvoiceInfo(SterlingDAO dao) {
-        String customerId = ReadData.acceptString("Customer Id: ");
-        int month = Integer.parseInt(ReadData.acceptString("Invoice Month: "));
-        int year = Integer.parseInt(ReadData.acceptString("Invoice Year: "));
-        String desc = ReadData.acceptString("Description: ");
-        dao.saveInvoice(new Invoice(customerId, month, year, desc));
+        String linkedCustomerId = ReadData.acceptString("Customer Id: ");
+        int billingMonth = Integer.parseInt(ReadData.acceptString("Invoice Month: "));
+        int billingYear = Integer.parseInt(ReadData.acceptString("Invoice Year: "));
+        String invoiceDescription = ReadData.acceptString("Description: ");
+        dao.saveInvoice(new Invoice(linkedCustomerId, billingMonth, billingYear, invoiceDescription));
     }
 }
