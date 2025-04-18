f149f78eddf6b3b66a2812d415bf854a5f2b8e07
SvaraShah <svarashah215@gmail.com>
2025-04-18 18:11:59 +0530

    Update OrderTester.java

--- PROBLEM3-main/BUISNESS REQ/tester/OrderTester.java
+++ PROBLEM3-main/BUISNESS REQ/tester/OrderTester.java
@@ -6,13 +6,13 @@ import util.ReadData;
 
 public class OrderTester {
     public static void readOrderInfo(SterlingDAO dao) {
-        String customerId = ReadData.acceptString("Customer Id: ");
-        String orderDate = ReadData.acceptString("Date Of Order(DD-MMM-YYYY): ");
-        String recipientName = ReadData.acceptString("Recipient Name: ");
-        String recipientAddress = ReadData.acceptString("Recipient Address: ");
-        String recipientCity = ReadData.acceptString("Recipient City (D, P, B, C): ");
-        float weight = Float.parseFloat(ReadData.acceptString("Courier Weight: "));
-        String description = ReadData.acceptString("Description: ");
-        dao.saveOrder(new Order(customerId, orderDate, recipientName, recipientAddress, recipientCity, weight, description));
+        String linkedCustomerId = ReadData.acceptString("Customer Id: ");
+        String dateOfOrder = ReadData.acceptString("Date Of Order(DD-MMM-YYYY): ");
+        String receiverName = ReadData.acceptString("Recipient Name: ");
+        String receiverAddress = ReadData.acceptString("Recipient Address: ");
+        String receiverCity = ReadData.acceptString("Recipient City (D, P, B, C): ");
+        float packageWeight = Float.parseFloat(ReadData.acceptString("Courier Weight: "));
+        String orderDescription = ReadData.acceptString("Description: ");
+        dao.saveOrder(new Order(linkedCustomerId, dateOfOrder, receiverName, receiverAddress, receiverCity, packageWeight, orderDescription));
     }
 }
