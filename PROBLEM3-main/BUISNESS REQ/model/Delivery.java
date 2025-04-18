e117a864d96d8b31b00403e42a37df221a3e977d
SvaraShah <svarashah215@gmail.com>
2025-04-18 18:03:13 +0530

    Update Delivery.java

--- PROBLEM3-main/BUISNESS REQ/model/Delivery.java
+++ PROBLEM3-main/BUISNESS REQ/model/Delivery.java
@@ -1,17 +1,17 @@
 package model;
 
 public class Delivery {
-    private String employeeId;
-    private String orderId;
-    private String deliveryDate;
-    private String status;
-    private String remarks;
+    private String assignedEmployeeId;
+    private String relatedOrderId;
+    private String dateOfDelivery;
+    private String deliveryStatus;
+    private String deliveryRemarks;
 
-    public Delivery(String employeeId, String orderId, String deliveryDate, String status, String remarks) {
-        this.employeeId = employeeId;
-        this.orderId = orderId;
-        this.deliveryDate = deliveryDate;
-        this.status = status;
-        this.remarks = remarks;
+    public Delivery(String assignedEmployeeId, String relatedOrderId, String dateOfDelivery, String deliveryStatus, String deliveryRemarks) {
+        this.assignedEmployeeId = assignedEmployeeId;
+        this.relatedOrderId = relatedOrderId;
+        this.dateOfDelivery = dateOfDelivery;
+        this.deliveryStatus = deliveryStatus;
+        this.deliveryRemarks = deliveryRemarks;
     }
 }
