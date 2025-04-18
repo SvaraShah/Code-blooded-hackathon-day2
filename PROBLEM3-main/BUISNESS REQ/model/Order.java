9ffb79273afdc47f281e8c99dc9886fc657c557a
SvaraShah <svarashah215@gmail.com>
2025-04-18 18:06:19 +0530

    Update Order.java

--- PROBLEM3-main/BUISNESS REQ/model/Order.java
+++ PROBLEM3-main/BUISNESS REQ/model/Order.java
@@ -1,21 +1,21 @@
 package model;
 
 public class Order {
-    private String customerId;
-    private String orderDate;
-    private String recipientName;
-    private String recipientAddress;
-    private String recipientCity;
-    private float weight;
-    private String description;
+    private String linkedCustomerId;
+    private String dateOfOrder;
+    private String receiverName;
+    private String receiverAddress;
+    private String receiverCity;
+    private float packageWeight;
+    private String orderDescription;
 
-    public Order(String customerId, String orderDate, String recipientName, String recipientAddress, String recipientCity, float weight, String description) {
-        this.customerId = customerId;
-        this.orderDate = orderDate;
-        this.recipientName = recipientName;
-        this.recipientAddress = recipientAddress;
-        this.recipientCity = recipientCity;
-        this.weight = weight;
-        this.description = description;
+    public Order(String linkedCustomerId, String dateOfOrder, String receiverName, String receiverAddress, String receiverCity, float packageWeight, String orderDescription) {
+        this.linkedCustomerId = linkedCustomerId;
+        this.dateOfOrder = dateOfOrder;
+        this.receiverName = receiverName;
+        this.receiverAddress = receiverAddress;
+        this.receiverCity = receiverCity;
+        this.packageWeight = packageWeight;
+        this.orderDescription = orderDescription;
     }
 }
